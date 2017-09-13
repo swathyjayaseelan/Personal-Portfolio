72534618bd0daabfc43a14122d158a77c5ba830f
swathyjayaseelan <swathyjayaseelan@gmail.com>
2017-09-13 16:18:49 -0700

    To deploy

--- webpack.config.js
+++ webpack.config.js
@@ -6,7 +6,7 @@ module.exports = {
     output: {
         path: path.resolve(__dirname),
         filename: 'bundle.js',
-        publicPath: '/'
+        publicPath: '/Personal-Portfolio'
     },
     module: {
         loaders: [
